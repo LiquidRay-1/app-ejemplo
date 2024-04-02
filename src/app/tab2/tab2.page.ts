5952939d7bc86559ce5eb0571a2225ecdc47ab75
Iván Tortosa <shadowheroe03@gmail.com>
2024-04-02 14:47:56 +0200

    Añadido
    Añadidos los cambios en los que se reflejan los

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Usuario } from '../models/usuario';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
@@ -18,6 +19,24 @@ export class Tab2Page implements OnInit {
       likes: 15,
       comentarios: 8,
       nota: "Carlos es un entusiasta de los deportes."
+    },
+    {
+      "avatarSrc": "assets/female-avatar.svg",
+      "nombre": "Ana",
+      "imagenSrc": "ruta-a-la-imagen-ana.jpg",
+      "mensaje": "¡Feliz día!",
+      "likes": 20,
+      "comentarios": 10,
+      "nota": "Ana es una amante de la naturaleza."
+    },
+    {
+      "avatarSrc": "assets/male-avatar.svg",
+      "nombre": "Pedro",
+      "imagenSrc": "ruta-a-la-imagen-pedro.jpg",
+      "mensaje": "¿Cómo están?",
+      "likes": 12,
+      "comentarios": 6,
+      "nota": "Pedro es un apasionado de la música."
     }
   ]
 
