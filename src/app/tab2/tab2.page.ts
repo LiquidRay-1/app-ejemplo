68709e9e0f42493396a72c5d3ef08914570aaf69
Iván Tortosa <shadowheroe03@gmail.com>
2024-04-02 11:45:10 +0200

    Añadido usuarios
    Base de datos local con usuarios y comentarios

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  users: any[] | undefined;
 
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
 }
